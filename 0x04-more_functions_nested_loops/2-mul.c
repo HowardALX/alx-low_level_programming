4ac29d1307f99c861e98599205a939721830b71f
ALXTIMHO <Timothy Tim. Howard>
2023-08-18 07:26:40 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * mul - function checks for checks for a number (0 through).
+  * @a: a - var
+  * @b: b - var
+  * Return: Always 0.
+  */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
