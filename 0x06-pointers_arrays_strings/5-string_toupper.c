59baf91c53bd61f2a2389851736c91f36d138a8f
ALXTIMHO <Timothy Tim. Howard>
2023-08-23 10:37:09 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - a func that changes all lowercase  to uppercase
+ * @n: pointer
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int q;
+
+	q = 0;
+	while (n[q] != '\0')
+	{
+		if (n[q] >= 'a' && n[q] <= 'z')
+			n[q] = n[q] - 32;
+		q++;
+	}
+	return  (n);
+}
